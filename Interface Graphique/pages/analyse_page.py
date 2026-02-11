2a5ea6e6d7587d26b59eae0610b217f893ccad48
Gasthorn <gaspard.bourdas@gmail.com>
2026-02-11 10:25:39 +0100

    Ajout dataType

--- Interface Graphique/pages/analyse_page.py
+++ Interface Graphique/pages/analyse_page.py
@@ -5,11 +5,32 @@ import numpy as np
 
 register_page(__name__, path="/analysis", name="Analyse")
 
+df_features = pd.read_csv("Data/ALL_FEATURES.csv", parse_dates=["date"])
+
 layout = html.Div(className="analysis-page", children = [
     # Titre animé
     html.Div(className="page-title", children=[
-        html.H1("Analyse d'Actifs en Temps Réel", className="glow-title"),
+        html.H1("Analyse effectuée par le modèle", className="glow-title"),
         html.Div(className="neon-underline")
     ]),
-    html.Div(className="data-container", children=[]),
+    html.Div(className="text-panel", children=[
+        html.H4("Données Utilisées", className="panel-title"),
+        html.Table(
+            className="lux-table split-table",
+            children=[
+                html.Thead(
+                    html.Tr([
+                        html.Th("Open"),
+                        html.Th("High"),
+                        html.Th("Low"),
+                        html.Th("Close"),
+                        html.Th("Volume"),
+                        html.Th("Volatility"),
+                        html.Th("RSI"),
+                        html.Th("MA 20"),
+                    ])
+                ),
+            ]
+        )
+    ]),
 ])
